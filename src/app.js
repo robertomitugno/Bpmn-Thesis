c1e919398e06b00b917a89adc6861a16b38caf55
mitug <roberto.mitugno@studio.unibo.it>
2024-04-22 19:32:39 +0200

    Add download functionality for BPMN file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import Modeler from 'bpmn-js/lib/Modeler';
-
 import PropertiesPanel from './properties-panel';
-
 import customModdleExtension from './moddle/custom.json';
-
 import diagramXML from './diagram.bpmn';
 
 const $modelerContainer = document.querySelector('#modeler-container');
 const $propertiesContainer = document.querySelector('#properties-container');
+const $downloadButton = document.querySelector('#download-button');
 
 const modeler = new Modeler({
   container: $modelerContainer,
@@ -25,3 +23,35 @@ const propertiesPanel = new PropertiesPanel({
 });
 
 modeler.importXML(diagramXML);
+
+document.addEventListener('keydown', function (event) {
+  if (!(event.ctrlKey || event.metaKey) || event.code !== 'KeyS') {
+    return;
+  }
+  event.preventDefault();
+
+  modeler.saveXML({ format: true }).then(function (result) {
+    download(result.xml, 'testing.bpmn', 'application/xml');
+  }).catch(function(err) {
+    console.error('Failed to save BPMN XML', err);
+  });
+});
+
+$downloadButton.addEventListener('click', function() {
+  modeler.saveXML({ format: true }).then(function (result) {
+    download(result.xml, 'testing.bpmn', 'application/xml');
+  }).catch(function(err) {
+    console.error('Failed to save BPMN XML', err);
+  });
+});
+
+function download(content, fileName, contentType) {
+  const a = document.createElement('a');
+  const file = new Blob([content], { type: contentType });
+
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+
+  URL.revokeObjectURL(a.href);
+}
