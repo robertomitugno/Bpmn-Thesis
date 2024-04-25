fac9dbb552c742dd4f107a58d3fa8a09fe5cc118
mitug <roberto.mitugno@studio.unibo.it>
2024-04-25 22:18:20 +0200

    Add list of Executos and Products

--- src/properties-panel/PropertiesView.js
+++ src/properties-panel/PropertiesView.js
@@ -1,234 +1,87 @@
 import { is } from 'bpmn-js/lib/util/ModelUtil';
-
 import React, { Component } from 'react';
-
 import './PropertiesView.css';
+import ExecutorsList from './ExecutorsList';
+import ProductList from './ProductList';
+import ElementProperties from './ElementProperties';
 
 
 export default class PropertiesView extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedElements: [],
-      element: null
-    };
-  }
+  state = {
+    selectedElements: [],
+    element: null,
+    products: [],
+    productName: '',
+    productId: 1
+  };
 
   componentDidMount() {
+    const { modeler } = this.props;
+    modeler.on('selection.changed', this.handleSelectionChange);
 
-    const {
-      modeler
-    } = this.props;
-
-    modeler.on('selection.changed', (e) => {
-
-      const {
-        element
-      } = this.state;
-
-      this.setState({
-        selectedElements: e.newSelection,
-        element: e.newSelection[0]
-      });
-    });
-
-
-    modeler.on('element.changed', (e) => {
-
-      const {
-        element
-      } = e;
-
-      const {
-        element: currentElement
-      } = this.state;
-
-      if (!currentElement) {
-        return;
-      }
-
-      // update panel, if currently selected element changed
-      if (element.id === currentElement.id) {
-        this.setState({
-          element
-        });
-      }
-
-    });
+    // Aggiorna l'elenco degli esecutori quando il componente viene montato
+    this.updateExecutorsList();
   }
 
+  componentWillUnmount() {
+    const { modeler } = this.props;
+    modeler.off('selection.changed', this.handleSelectionChange);
+  }
+
+  handleSelectionChange = e => {
+    this.setState({
+      selectedElements: e.newSelection,
+      element: e.newSelection[0]
+    });
+  };
+
+  updateExecutorsList = () => {
+    const { modeler } = this.props;
+    modeler.on('elements.changed', () => {
+      this.refs.executorsList.updateActivities();
+    });
+    this.refs.executorsList.updateActivities();
+  };
+
+  handleProductNameChange = event => {
+    this.setState({ productName: event.target.value });
+  };
+
+  handleAddProduct = () => {
+    const { productName, productId } = this.state;
+    if (productName.trim() !== '') {
+      const newProduct = { id: productId, name: productName };
+      this.setState(prevState => ({
+        products: [...prevState.products, newProduct],
+        productName: '',
+        productId: prevState.productId + 1
+      }));
+    }
+  };
   render() {
-
-    const {
-      modeler
-    } = this.props;
-
-    const {
-      selectedElements,
-      element
-    } = this.state;
+    const { modeler } = this.props;
+    const { selectedElements, element, products, productName } = this.state;
 
     return (
       <div>
-
-        {
-          selectedElements.length === 1
-            && <ElementProperties modeler={ modeler } element={ element } />
-        }
-
-        {
-          selectedElements.length === 0
-            && <span>Please select an element.</span>
-        }
-
-        {
-          selectedElements.length > 1
-            && <span>Please select a single element.</span>
-        }
+        {selectedElements.length === 1 && (
+          <div>
+            <ElementProperties modeler={modeler} element={element} />
+          </div>
+        )}
+        {selectedElements.length === 0 && (
+          <div>
+            <ExecutorsList ref="executorsList" modeler={modeler} />
+            <ProductList
+              products={products}
+              productName={productName}
+              onProductNameChange={this.handleProductNameChange}
+              onAddProduct={this.handleAddProduct}
+            />
+          </div>
+        )}
+        {selectedElements.length > 1 && <span>Please select a single element.</span>}
       </div>
     );
   }
-
-}
-
-
-function ElementProperties(props) {
-
-  let {
-    element,
-    modeler
-  } = props;
-
-  if (element.labelTarget) {
-    element = element.labelTarget;
-  }
-
-  function updateName(name) {
-    const modeling = modeler.get('modeling');
-
-    modeling.updateLabel(element, name);
-  }
-
-  function updateTopic(topic) {
-    const modeling = modeler.get('modeling');
-
-    modeling.updateProperties(element, {
-      'custom:topic': topic
-    });
-  }
-
-  function makeMessageEvent() {
-
-    const bpmnReplace = modeler.get('bpmnReplace');
-
-    bpmnReplace.replaceElement(element, {
-      type: element.businessObject.$type,
-      eventDefinitionType: 'bpmn:MessageEventDefinition'
-    });
-  }
-
-  function makeServiceTask(name) {
-    const bpmnReplace = modeler.get('bpmnReplace');
-
-    bpmnReplace.replaceElement(element, {
-      type: 'bpmn:ServiceTask'
-    });
-  }
-
-  function attachTimeout() {
-    const modeling = modeler.get('modeling');
-    const autoPlace = modeler.get('autoPlace');
-    const selection = modeler.get('selection');
-
-    const attrs = {
-      type: 'bpmn:BoundaryEvent',
-      eventDefinitionType: 'bpmn:TimerEventDefinition'
-    };
-
-    const position = {
-      x: element.x + element.width,
-      y: element.y + element.height
-    };
-
-    const boundaryEvent = modeling.createShape(attrs, position, element, { attach: true });
-
-    const taskShape = append(boundaryEvent, {
-      type: 'bpmn:Task'
-    });
-
-    selection.select(taskShape);
-  }
-
-  function isTimeoutConfigured(element) {
-    const attachers = element.attachers || [];
-
-    return attachers.some(e => hasDefinition(e, 'bpmn:TimerEventDefinition'));
-  }
-
-  function append(element, attrs) {
-
-    const autoPlace = modeler.get('autoPlace');
-    const elementFactory = modeler.get('elementFactory');
-
-    var shape = elementFactory.createShape(attrs);
-
-    return autoPlace.append(element, shape);
-  };
-
-  return (
-    <div className="element-properties" key={ element.id }>
-      <fieldset>
-        <label>id</label>
-        <span>{ element.id }</span>
-      </fieldset>
-
-      <fieldset>
-        <label>name</label>
-        <input value={ element.businessObject.name || '' } onChange={ (event) => {
-          updateName(event.target.value)
-        } } />
-      </fieldset>
-
-      {
-        is(element, 'custom:TopicHolder') &&
-          <fieldset>
-            <label>topic (custom)</label>
-            <input value={ element.businessObject.get('custom:topic') } onChange={ (event) => {
-              updateTopic(event.target.value)
-            } } />
-          </fieldset>
-      }
-
-      <fieldset>
-        <label>actions</label>
-
-        {
-          is(element, 'bpmn:Task') && !is(element, 'bpmn:ServiceTask') &&
-            <button onClick={ makeServiceTask }>Make Service Task</button>
-        }
-
-        {
-          is(element, 'bpmn:Event') && !hasDefinition(element, 'bpmn:MessageEventDefinition') &&
-            <button onClick={ makeMessageEvent }>Make Message Event</button>
-        }
-
-        {
-          is(element, 'bpmn:Task') && !isTimeoutConfigured(element) &&
-            <button onClick={ attachTimeout }>Attach Timeout</button>
-        }
-      </fieldset>
-    </div>
-  );
-}
-
-
-// helpers ///////////////////
-
-function hasDefinition(event, definitionType) {
-
-  const definitions = event.businessObject.eventDefinitions || [];
-
-  return definitions.some(d => is(d, definitionType));
 }
